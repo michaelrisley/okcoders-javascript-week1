2e2f719166e9b99cfeb73e0e3797d12b9575d0ee
Michael A. Risley <michael.risley@gmail.com>
2018-03-10 09:42:59 -0600

    Week 1, completed in Week 2

---
+++ businesscard.js
@@ -0,0 +1,11 @@
+var name = 'Michael A. Risley'
+var company = 'Michael A. Risley, Attorney & Counselor at Law PLLC'
+var phone = '405.801.2116'
+var email = 'michael@michaelrisley.com'
+
+console.log('-------------------')
+console.log(name)
+console.log(company)
+console.log(phone)
+console.log(email)
+console.log('-------------------')
---
+++ test.js
@@ -0,0 +1,16 @@
+var age = 36
+var old = 75
+
+while (age != old)
+	{
+	console.log("How old are you?");
+	console.log("You are " +age);
+	console.log("You are not old!");
+	age++; 
+	console.log ("");
+	}
+
+console.log("How old are you?");
+console.log("You are " +age);
+console.log("You are old!");
+	
